b2a38b4d8124ea10216613627e86782a86be9aa6
oricalvo <ori.calvo@gmail.com>
2015-10-28 11:18:06 +0200

    More

---
+++ 01 Whats The Big Idea/GettingStarted/GettingStarted/Controllers/ContactController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GettingStarted.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Index(string userName, string email)
+        {
+            DAL dal = new DAL();
+            ContactModel[] contacts = dal.GetAllContacts().Select(c => new ContactModel()
+            {
+                UserName = c.UserName,
+                EMail = c.EMail,
+                IsOldest = true,
+            }).ToArray();
+
+            return View(contacts);
+        }
+
+        public ActionResult Create(string userName, string email)
+        {
+            DAL dal = new DAL();
+            dal.AddContact(userName, email);
+
+            return RedirectToAction("Index");
+        }
+    }
+
+    public class ContactModel
+    {
+        public string UserName { get; set; }
+        public string EMail { get; set; }
+        public bool IsOldest { get; set; }
+    }
+}
---
+++ 01 Whats The Big Idea/GettingStarted/GettingStarted/DAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GettingStarted
+{
+    public class DAL
+    {
+        private static List<Contact> contacts = new List<Contact>()
+        {
+            new Contact() {UserName="Ori", EMail="ori@gmail.com" },
+            new Contact() {UserName="Roni", EMail="roni@gmail.com" },
+        };
+
+        public void AddContact(string userName, string email)
+        {
+            contacts.Add(new Contact()
+            {
+                UserName = userName,
+                EMail = email,
+            });
+        }
+
+        public Contact[] GetAllContacts()
+        {
+            return contacts.ToArray();
+        }
+    }
+
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string EMail { get; set; }
+    }
+}
